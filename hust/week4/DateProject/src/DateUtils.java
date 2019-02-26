b550a8b7ad5261a7f2c5b674cd2adfb786622d3e
duchunter <duc71098@gmail.com>
2019-02-26 10:13:08 +0700

    Splash

--- hust/week4/DateProject/src/DateUtils.java
+++ hust/week4/DateProject/src/DateUtils.java
@@ -1,9 +1,33 @@
 public class DateUtils {
-  public static int compareDate() {
+  public static int compareDate(MyDate date1, MyDate date2) {
+    if (date1.getYear() > date2.getYear()) {
+      return 1;
+    }
+
+    if (date1.getYear() < date2.getYear()) {
+      return -1;
+    }
+
+    if (date1.getMonth() > date2.getMonth()) {
+      return 1;
+    }
+
+    if (date1.getMonth() < date2.getMonth()) {
+      return -1;
+    }
+
+    if (date1.getDay() > date2.getDay()) {
+      return 1;
+    }
+
+    if (date1.getDay() < date2.getDay()) {
+      return -1;
+    }
+
     return 0;
   }
 
-  public static void sortDate() {
-
+  public static void sortDate(MyDate[] dateList) {
+    
   }
 }
