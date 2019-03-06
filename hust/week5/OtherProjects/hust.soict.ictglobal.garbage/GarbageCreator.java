d82751d53cd42187b6f108df6a881737459e5adf
duchunter <duc71098@gmail.com>
2019-03-06 22:59:47 +0700

    hw week 5

--- hust/week5/OtherProjects/hust.soict.ictglobal.garbage/GarbageCreator.java
+++ hust/week5/OtherProjects/hust.soict.ictglobal.garbage/GarbageCreator.java
@@ -1,3 +1,15 @@
+import java.io.FileReader;
+
 public class GarbageCreator {
-  
+  public static void main(String[] args) throws Exception  {
+    FileReader fr = new FileReader("test.txt");
+
+    int i;
+    String str = "";
+    while ((i = fr.read()) != -1) {
+      str += (char) i;
+    }
+
+    System.out.println(str);
+  }
 }
--- hust/week5/OtherProjects/hust.soict.ictglobal.garbage/NoGarbage.java
+++ hust/week5/OtherProjects/hust.soict.ictglobal.garbage/NoGarbage.java
@@ -1,3 +1,15 @@
+import java.io.FileReader;
+
 public class NoGarbage {
-  
+  public static void main(String[] args) throws Exception {
+    FileReader fr = new FileReader("test.txt");
+
+    int i;
+    StringBuilder sb = new StringBuilder();
+    while ((i = fr.read()) != -1) {
+      sb.append((char) i);
+    }
+
+    System.out.println(sb.toString());
+  }
 }
